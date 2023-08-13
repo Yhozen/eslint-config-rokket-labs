03cb829cc62d2e82793d1f45c11e2954c3523910
Gabriel Pérez <gabriel@garox.org>
2023-08-13 14:33:31 +0200

    add ui example and tsconfig

---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['drimo/react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+}
---
+++ packages/ui/index.tsx
@@ -0,0 +1,4 @@
+// component exports
+export * from './Button'
+// eslint-disable-next-line padding-line-between-statements
+export * from './Header'
